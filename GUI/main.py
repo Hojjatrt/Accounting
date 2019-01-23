25de2464200c619457c2c268e93cd582857a1cc6
Hojjat Rafat <35579662+Hojjatrt@users.noreply.github.com>
2019-01-24 01:12:12 +0330

    add percent to main, updata qtable, fix Qmassage box bug!

--- GUI/main.py
+++ GUI/main.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import *
 from GUI.UI_Files.add_product import Ui_Dialog_add
 from GUI.UI_Files.main_window import Ui_MainWindow
 from database.Database import Database
-from Models.Accounting import Accounting
+from Models.Models import Product
 
 
 class AddDialog(QDialog):
@@ -27,17 +27,17 @@ class AddDialog(QDialog):
         self._add_dialog.btn_add_cancel.clicked.connect(self.exit)
         self._add_dialog.btn_add_save.clicked.connect(self.add_product)
         self._add_dialog.txt_purchase_price.textChanged.connect(self.txt_pprice_change)
-        self._add_dialog.txt_sales_price.textChanged.connect(self.txt_sprice_change)
+        self._add_dialog.txt_sales_percent.textChanged.connect(self.txt_percent_change)
 
     def txt_pprice_change(self):
         p_price = self._add_dialog.txt_purchase_price.text()
         if not p_price.isnumeric():
             self._add_dialog.txt_purchase_price.setText(p_price[:len(p_price)-1])
 
-    def txt_sprice_change(self):
-        s_price = self._add_dialog.txt_sales_price.text()
-        if not s_price.isnumeric():
-            self._add_dialog.txt_sales_price.setText(s_price[:len(s_price)-1])
+    def txt_percent_change(self):
+        percent = self._add_dialog.txt_sales_percent.text()
+        if not percent.isnumeric():
+            self._add_dialog.txt_sales_percent.setText(percent[:len(percent)-1])
 
     def add_product(self):
         global db
@@ -48,31 +48,25 @@ class AddDialog(QDialog):
             return
         p_price = self._add_dialog.txt_purchase_price.text()
         if p_price == '':
-            QMessageBox.question(self.dialog, 'خطا', " قیمت خرید محصول باید وارد شود!",
-                                 QMessageBox.Ok, QMessageBox.Ok)
+            QMessageBox.information(self, 'خطا', " قیمت خرید محصول باید وارد شود!",
+                                    QMessageBox.Ok, QMessageBox.Ok)
             return
-        s_price = self._add_dialog.txt_sales_price.text()
-        if s_price == '':
-            QMessageBox.question(self.dialog, 'خطا', "قیمت فروش محصول باید وارد شود!",
-                                 QMessageBox.Ok, QMessageBox.Ok)
+        percent = self._add_dialog.txt_sales_percent.text()
+        if percent == '':
+            QMessageBox.information(self, 'خطا', "درصد فروش محصول باید وارد شود!",
+                                    QMessageBox.Ok, QMessageBox.Ok)
             return
         p_price = int(p_price)
-        s_price = int(s_price)
-        if p_price > s_price:
-            QMessageBox.question(self.dialog, 'خطا', "قیمت خرید نباید بیشتر از قیمت فروش باشد!",
-                                 QMessageBox.Ok, QMessageBox.Ok)
-            return
-        print('here is')
-        acc = Accounting(name, p_price, s_price)
-        print('here is 2')
-        acc.insert(db)
-        print('here is 3')
-        QMessageBox.information(self.dialog, 'افزودن', "محصول با موفقیت افزوده شد!",
-                             QMessageBox.Ok, QMessageBox.Ok)
+        percent = int(percent)
 
-        self._add_dialog.txt_product_name.text('')
-        self._add_dialog.txt_sales_price.text('')
-        self._add_dialog.txt_purchase_price.text('')
+        acc = Product(name, p_price, percent=percent)
+        acc.insert(db)
+        QMessageBox.information(self, 'افزودن', "محصول با موفقیت افزوده شد!",
+                                QMessageBox.Ok, QMessageBox.Ok)
+
+        self._add_dialog.txt_product_name.setText('')
+        self._add_dialog.txt_sales_percent.setText('')
+        self._add_dialog.txt_purchase_price.setText('')
         self._add_dialog.txt_product_name.setFocus()
 
     # method exit
@@ -88,14 +82,16 @@ class MainWindow(QMainWindow):
         self._window = Ui_MainWindow()
         self._window.setupUi(self)
         self._window.retranslateUi(self)
-        self._add_dialog = AddDialog(self._window)
+        self._add_dialog = AddDialog(self)
 
         # Initialize some other variables.
         self.table_rows = self._window.tableWidget.rowCount()
         header = self._window.tableWidget.horizontalHeader()
         header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
-        header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeToContents)
         header.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(3, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(4, QtWidgets.QHeaderView.Stretch)
 
         self.timer = QtCore.QTimer()
         self.timer.setInterval(500)
@@ -111,6 +107,7 @@ class MainWindow(QMainWindow):
         self._window.txt_main_search_name.textChanged.connect(self.search)
         self._window.menu_exit.triggered.connect(self.exit)
         self._window.btn_main_exit.clicked.connect(self.exit)
+        self._window.btn_main_add.clicked.connect(self.show_add_dialog)
         self._window.menu_add.triggered.connect(self.show_add_dialog)
 
     def displayTime(self):
@@ -120,26 +117,27 @@ class MainWindow(QMainWindow):
         self._window.tableWidget.setRowCount(0)
         global db
         text = self._window.txt_main_search_name.text()
-        print(text)
         if text == '':
             data = db.select()
-            print(data)
             for d in data:
                 self.table_rows = self._window.tableWidget.rowCount()
                 self._window.tableWidget.insertRow(self.table_rows)
                 self._window.tableWidget.setItem(self.table_rows, 0, QTableWidgetItem(d[1]))
-                self._window.tableWidget.setItem(self.table_rows, 1, QTableWidgetItem(str(d[2])))
-                self._window.tableWidget.setItem(self.table_rows, 2, QTableWidgetItem(str(d[3])))
+                self._window.tableWidget.setItem(self.table_rows, 1, QTableWidgetItem(str(d[0])))
+                self._window.tableWidget.setItem(self.table_rows, 2, QTableWidgetItem(str(d[2])))
+                self._window.tableWidget.setItem(self.table_rows, 3, QTableWidgetItem(str(d[4])))
+                self._window.tableWidget.setItem(self.table_rows, 4, QTableWidgetItem(str(d[3])))
 
         else:
             data = db.select(text)
-            print(data)
             for d in data:
                 self.table_rows = self._window.tableWidget.rowCount()
                 self._window.tableWidget.insertRow(self.table_rows)
                 self._window.tableWidget.setItem(self.table_rows, 0, QTableWidgetItem(d[1]))
-                self._window.tableWidget.setItem(self.table_rows, 1, QTableWidgetItem(str(d[2])))
-                self._window.tableWidget.setItem(self.table_rows, 2, QTableWidgetItem(str(d[3])))
+                self._window.tableWidget.setItem(self.table_rows, 1, QTableWidgetItem(str(d[0])))
+                self._window.tableWidget.setItem(self.table_rows, 2, QTableWidgetItem(str(d[2])))
+                self._window.tableWidget.setItem(self.table_rows, 3, QTableWidgetItem(str(d[4])))
+                self._window.tableWidget.setItem(self.table_rows, 4, QTableWidgetItem(str(d[3])))
 
     def show_add_dialog(self):
         self._add_dialog.show()
