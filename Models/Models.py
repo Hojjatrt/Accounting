961bf0631df4c968e48d68296bf198fb2a9ace32
Hojjat Rafat <35579662+Hojjatrt@users.noreply.github.com>
2019-01-30 01:07:34 +0330

    add accessories variable to product model and change methods

--- Models/Models.py
+++ Models/Models.py
@@ -1,10 +1,15 @@
 class Product:
-    def __init__(self, name='test', purchase_price=0, percent=5, sales_price=0):
+    def __init__(self, name='test', purchase_price=0, percent=0, accessories=False, sales_price=0,):
         self.id = 0
         self.name = name
         self.purchase_price = purchase_price
-        self.percent = percent
-        self.sales_price = int((self.percent / 100.0 * self.purchase_price) + self.purchase_price)
+        self.accessories = accessories
+        if self.accessories:
+            self.sales_price = sales_price
+            self.percent = int((self.sales_price - self.purchase_price) / self.purchase_price * 100)
+        else:
+            self.percent = percent
+            self.sales_price = int((self.percent / 100.0 * self.purchase_price) + self.purchase_price)
 
     def insert(self, db):
         db.insert(self)
@@ -13,7 +18,8 @@ class Product:
     def search(db, id):
         product = db.search(id)
         if product:
-            p = Product(name=product[1], purchase_price=product[2], percent=product[4])
+            p = Product(name=product[1], purchase_price=product[2], percent=product[4], accessories=product[5],
+                        sales_price=product[3])
             p.id = product[0]
             return p
         else:
